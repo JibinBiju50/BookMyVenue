f3ae98f255811a9eba2e2d6b21a3d2d2d1b1d9ac
Jibin <Jibinchelakattu555@gmail.com>
2026-07-08 14:30:36 +0530

    Rewrite cookie options

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const baseCookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: "lax",
+  sameSite: "none",
   path: "/",
 };
 
